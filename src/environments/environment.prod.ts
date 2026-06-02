602da902ff149e5f11d4106b1ff9b3c5bd8d478c
Papá Gato <carlosmongeorihuela.2003@gmail.com>
2026-06-02 02:27:50 -0500

    cambio de url back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://nexocpm-api.azurewebsites.net/api',
+  apiUrl: 'https://nexocpm-api-dhepcndqdkfdc8ep.canadacentral-01.azurewebsites.net/api',
 };
